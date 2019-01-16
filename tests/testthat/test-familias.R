ccb6b6e9df712c6cc3eb1fb9997d9ccc1b5d9658
magnusdv <magnusdv@medisin.uio.no>
2019-01-16 10:43:56 +0100

    Add test for singleton conversion

--- tests/testthat/test-familias.R
+++ tests/testthat/test-familias.R
@@ -22,16 +22,32 @@ test_that("Familias2ped() converts fullsib pedigree", {
 
 })
 
-test_that("Familias2ped() converts singletons", {
+test_that("Familias2ped() converts a list of singletons", {
 
   famped = list(id = 3:1, findex = c(0,0,0),  mindex = c(0,0,0),  sex = c("male", "male", "female"))
   class(famped) = "FamiliasPedigree"
 
   ped = Familias2ped(famped, NULL, NULL)
-  expect_identical(ped, list('_comp1' = singleton(3, famid='_comp1'),
-                             '_comp2' = singleton(2, famid='_comp2'),
-                             '_comp3' = singleton(1, sex=2, famid='_comp3')))
+  expect_equivalent(ped, list('_comp1' = singleton(3, famid='_comp1'),
+                              '_comp2' = singleton(2, famid='_comp2'),
+                              '_comp3' = singleton(1, sex=2, famid='_comp3')))
 
+  #datamatrix = data.frame(m1.1 = c(1,"A",NA), m1.2 = c(1,"B",1), row.names = 1:3)
+  #ped = Familias2ped(famped, datamatrix, NULL)
+})
+
+test_that("Familias2ped() converts a single singleton", {
+
+  famped = list(id = 1, findex = 0,  mindex = 0,  sex = "male")
+  class(famped) = "FamiliasPedigree"
+
+  datamatrix = data.frame(m1.1 = "A", m1.2 = "B", row.names = 1)
+  ped = Familias2ped(famped, datamatrix, NULL)
+
+  true = singleton(1)
+  true = addMarkers(true, marker(true, `1` = c("A", "B")))
+
+  expect_identical(ped, true)
 })
 
 test_that("Familias2ped() reverses pedlikCompare:::ped2Familias()", {
