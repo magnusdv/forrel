25348ef45cb29c9304780e7872ce535097f78000
magnusdv <magnusdv@medisin.uio.no>
2019-06-08 22:52:10 +0200

    `exclusionPower()` plotting simplified

--- R/exclusionPower.R
+++ R/exclusionPower.R
@@ -182,41 +182,43 @@ exclusionPower = function(ped_claim, ped_true, ids, markerindex = NULL,
   }
 
   if (isTRUE(plot) || plot == "plot_only") {
-    op = par(oma = c(0, 0, 3, 0), xpd = NA)
-    widths = ifelse(sapply(c(ped_claim, ped_true), is.singleton), 1, 2)
-    claim_ratio = sum(widths[1:N_claim]) / sum(widths)
-    layout(rbind(1:N), widths = widths)
-    has_genotypes = length(known_genotypes) > 0
-    for (i in 1:N) {
-      if (i <= N_claim) {
-        x = ped_claim[[i]]
-        avail = ids_claim[[i]]
-        mm = if (has_genotypes) partial_claim[[i]] else NULL
-      } else {
-        x = ped_true[[i - N_claim]]
-        avail = ids_true[[i - N_claim]]
-        mm = if (has_genotypes) partial_true[[i - N_claim]] else NULL
-      }
-
-      cols = ifelse(labels(x) %in% avail, 2, 1)
-      plot(x, marker = mm, col = cols, margin = c(2, 4, 2, 4), title = "")
-    }
-
-    mtext("Claim", outer = TRUE, at = claim_ratio / 2)
-    mtext("True", outer = TRUE, at = 0.5 + claim_ratio / 2)
-    rect(
-      grconvertX(0.02, from = "ndc"),
-      grconvertY(0.02, from = "ndc"),
-      grconvertX(claim_ratio - 0.02, from = "ndc"),
-      grconvertY(0.98, from = "ndc")
-    )
-    rect(
-      grconvertX(claim_ratio + 0.02, from = "ndc"),
-      grconvertY(0.02, from = "ndc"),
-      grconvertX(0.98, from = "ndc"),
-      grconvertY(0.98, from = "ndc")
-    )
-    par(op)
+    plotPedList(list(ped_claim, ped_true), new = T,
+                frametitles = c("Claim", "True"), shaded = ids)
+    # op = par(oma = c(0, 0, 3, 0), xpd = NA)
+    # widths = ifelse(sapply(c(ped_claim, ped_true), is.singleton), 1, 2)
+    # claim_ratio = sum(widths[1:N_claim]) / sum(widths)
+    # layout(rbind(1:N), widths = widths)
+    # has_genotypes = length(known_genotypes) > 0
+    # for (i in 1:N) {
+    #   if (i <= N_claim) {
+    #     x = ped_claim[[i]]
+    #     avail = ids_claim[[i]]
+    #     mm = if (has_genotypes) partial_claim[[i]] else NULL
+    #   } else {
+    #     x = ped_true[[i - N_claim]]
+    #     avail = ids_true[[i - N_claim]]
+    #     mm = if (has_genotypes) partial_true[[i - N_claim]] else NULL
+    #   }
+    #
+    #   cols = ifelse(labels(x) %in% avail, 2, 1)
+    #   plot(x, marker = mm, col = cols, margin = c(2, 4, 2, 4), title = "")
+    # }
+    #
+    # mtext("Claim", outer = TRUE, at = claim_ratio / 2)
+    # mtext("True", outer = TRUE, at = 0.5 + claim_ratio / 2)
+    # rect(
+    #   grconvertX(0.02, from = "ndc"),
+    #   grconvertY(0.02, from = "ndc"),
+    #   grconvertX(claim_ratio - 0.02, from = "ndc"),
+    #   grconvertY(0.98, from = "ndc")
+    # )
+    # rect(
+    #   grconvertX(claim_ratio + 0.02, from = "ndc"),
+    #   grconvertY(0.02, from = "ndc"),
+    #   grconvertX(0.98, from = "ndc"),
+    #   grconvertY(0.98, from = "ndc")
+    # )
+    # par(op)
     if (plot == "plot_only")
       return()
   }
