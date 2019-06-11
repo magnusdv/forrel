ec94725f08dca21c9143f25153752a421430a035
magnusdv <magnusdv@medisin.uio.no>
2019-06-11 09:56:39 +0200

    More work on exlusionPower()

--- R/exclusionPower.R
+++ R/exclusionPower.R
@@ -127,6 +127,8 @@
 exclusionPower = function(ped_claim, ped_true, ids, markerindex = NULL,
                           alleles = NULL, afreq = NULL, known_genotypes = list(),
                           Xchrom = FALSE, plot = TRUE) {
+
+  st = proc.time()
   if (is.ped(ped_claim))
     ped_claim = list(ped_claim)
   if (is.ped(ped_true))
@@ -181,63 +183,65 @@ exclusionPower = function(ped_claim, ped_true, ids, markerindex = NULL,
     })
   }
 
+  allgenos = allGenotypes(length(alleles))
+
   if (isTRUE(plot) || plot == "plot_only") {
-    plotPedList(list(ped_claim, ped_true), new = T,
-                frametitles = c("Claim", "True"), shaded = ids)
-    # op = par(oma = c(0, 0, 3, 0), xpd = NA)
-    # widths = ifelse(sapply(c(ped_claim, ped_true), is.singleton), 1, 2)
-    # claim_ratio = sum(widths[1:N_claim]) / sum(widths)
-    # layout(rbind(1:N), widths = widths)
-    # has_genotypes = length(known_genotypes) > 0
-    # for (i in 1:N) {
-    #   if (i <= N_claim) {
-    #     x = ped_claim[[i]]
-    #     avail = ids_claim[[i]]
-    #     mm = if (has_genotypes) partial_claim[[i]] else NULL
-    #   } else {
-    #     x = ped_true[[i - N_claim]]
-    #     avail = ids_true[[i - N_claim]]
-    #     mm = if (has_genotypes) partial_true[[i - N_claim]] else NULL
-    #   }
-    #
-    #   cols = ifelse(labels(x) %in% avail, 2, 1)
-    #   plot(x, marker = mm, col = cols, margin = c(2, 4, 2, 4), title = "")
-    # }
-    #
-    # mtext("Claim", outer = TRUE, at = claim_ratio / 2)
-    # mtext("True", outer = TRUE, at = 0.5 + claim_ratio / 2)
-    # rect(
-    #   grconvertX(0.02, from = "ndc"),
-    #   grconvertY(0.02, from = "ndc"),
-    #   grconvertX(claim_ratio - 0.02, from = "ndc"),
-    #   grconvertY(0.98, from = "ndc")
-    # )
-    # rect(
-    #   grconvertX(claim_ratio + 0.02, from = "ndc"),
-    #   grconvertY(0.02, from = "ndc"),
-    #   grconvertX(0.98, from = "ndc"),
-    #   grconvertY(0.98, from = "ndc")
-    # )
-    # par(op)
+    plotPedList(list(ped_claim, ped_true),
+                new = T, frametitles = c("Claim", "True"), shaded = ids)
+
     if (plot == "plot_only")
       return()
   }
 
-  p.g = Reduce("%o%", lapply(which(lengths(ids_true) > 0), function(i)
-    oneMarkerDistribution(
-      ped_true[[i]],
-      ids = ids_true[[i]],
-      partialmarker = partial_true[[i]],
-      verbose = F
-    )))
-  I.g = Reduce("%o%", lapply(which(lengths(ids_claim) > 0), function(i)
+  ### Identify genotype combinations incompatible with "claim"
+  # Relevant components
+  compIdx = which(lengths(ids_claim) > 1) # TODO change to 1!
+
+  # Incopatible combinations in each component
+  I.g.list = lapply(compIdx, function(i)
     oneMarkerDistribution(
       ped_claim[[i]],
       ids = ids_claim[[i]],
       partialmarker = partial_claim[[i]],
-      verbose = F
-    ) == 0))
+      verbose = F,
+      eliminate=0
+    ) == 0)
+
+  # outer product OR
+  outerOR = function(x,y) outer(x, y, FUN = `|`)
+  I.g = Reduce(outerOR, I.g.list)
+
+  # If no incompatibilities, return 0
+  if(!any(I.g))
+    return(0)
+
+  # Extract the TRUE positions (= incompatible combinations)
+  # Columns are named with those `ids` with contribution in I.g.
+  incomp.grid = which(I.g, arr.ind = T, useNames = F)
+  colnames(incomp.grid) = ids0 = unlist(ids_claim[compIdx])
+
+  ### In the true ped: Sum probs of the incompat combinations
+  # Where are the contributing indivs
+  ids0_true = lapply(ped_true, function(x) as.character(ids0[ids0 %in% labels(x)]))
+
+  # Relevant components
+  trueCompIdx = which(lengths(ids0_true) > 0) # (This must be 0)
+
+
+  p.g.list = lapply(trueCompIdx, function(i) {
+    ids.i = ids0_true[[i]]
+    grid.i = unique.matrix(incomp.grid[, ids.i, drop = F])
+
+    oneMarkerDistribution(
+      ped_true[[i]], ids.i,
+      partialmarker = partial_true[[i]],
+      grid.subset = grid.i,
+      verbose = F,
+      eliminate = 1
+    )
+  })
+
+  p.g = Reduce("%o%", p.g.list)
 
   sum(p.g * I.g)
 }
-
