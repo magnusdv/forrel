912723aa12a2fbb07d679ce9d490bbd4f5462496
Elias Hernandis <eliashernandis@gmail.com>
2019-06-17 17:49:34 +0200

    Load pedigree custom pedigree files and allele frequency databases.

--- inst/exclusionPowerUI/server.R
+++ inst/exclusionPowerUI/server.R
@@ -9,28 +9,52 @@
 
 library(shiny)
 
-pedigreeFromUI = function(pedigreeID) {
+pedigreeFromUI = function(pedigreeID, pedfile = NULL) {
   if (pedigreeID == 'nucPed-1s') {
     return(nuclearPed(1, father = "Father", mother = "Mother", children = c("Son")))
   } else if (pedigreeID == 'nucPed-1d') {
     return (nuclearPed(1, sex = 2, father = "Father", mother = "Mother", children = c("Daughter")))
+  } else if (pedigreeID == 'pedfile') {
+    if (is.null(pedfile)) return();
+
+    return(as.ped(read.table(pedfile$datapath)))
   } else if (pedigreeID == "unrelated") {
     return(list(singleton(id = 1), singleton(id = 3, sex = 2)))
     }
 }
 
 shinyServer(function(input, output, session) {
+  # update list of persons available for genotyping when the user chooses a pedigree
   observe({
-    pedigree = pedigreeFromUI(input$pedClaim)
-    updateCheckboxGroupInput(session, "ids", choices = pedigree$ID)
-    output$pedClaimPlot <- renderPlot({
-      plot(pedigree)
-    })
+    pedigree = pedigreeFromUI(input$pedClaim, input$pedClaimFile)
+    if (!is.null(pedigree)) {
+      updateCheckboxGroupInput(session, "ids", choices = pedigree$ID)
+
+      # render the pedigree plot when the user chooses a Claim pedigree
+      output$pedClaimPlot <- renderPlot({
+        plot(pedigree)
+      })
+    }
+
+    if (!is.null(input$pedClaimFile)) {
+      updateSelectInput(session, 'pedClaimFile', selected = 'pedfile')
+    }
   })
 
+  # render the pedigree plot when the user chooses a True pedigree
   output$pedTruePlot <- renderPlot({
-    pedigree = pedigreeFromUI(input$pedTrue)
+    pedigree = pedigreeFromUI(input$pedTrue, input$pedTrueFile)
     plot(pedigree)
   })
 
+  # load frequency file
+  frequencyDB <- reactiveVal()
+  output$frequencyDbSummary <- renderText({
+    if (is.null(input$frequencyDbFile)) {
+      return("Load a frequency database file")
+    } else {
+      frequencyDB(read.table(input$frequencyDbFile$datapath))
+      return(sprintf("Loaded frequency database with %d markers", ncol(frequencyDB())))
+    }
+  })
 })
--- inst/exclusionPowerUI/ui.R
+++ inst/exclusionPowerUI/ui.R
@@ -14,20 +14,40 @@ defaultPedigrees = c("Nuclear family (1 son)" = "nucPed-1s",
                      "Nuclear family (2 sons)" = "nucPed-2s",
                      "Nuclear family (2 daughters)" = "nucPed-2d",
                      "Nuclear family (1 son, 1 daughter)" = "NucPed-1s-1d",
-                     "Unrelated" = "unrelated")
+                     "Unrelated" = "unrelated",
+                     "Custom (from .ped file)" = "pedfile")
 
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
+  includeCSS('styles.css'),
   fluidRow(
     column(12, titlePanel('Exclusion Power'))
   ),
   fluidRow(
+    column(12, h3('Step 1. Choose pedigrees'))
+  ),
+  fluidRow(
     column(6,
-           selectInput("pedClaim", "Claim pedigree", defaultPedigrees),
+           selectInput("pedClaim", "Claim pedigree", defaultPedigrees, width = '100%'),
+           HTML('<p class="text-center">- or -</p>'),
+           fileInput('pedClaimFile', ".ped file",  width = '100%'),
            plotOutput('pedClaimPlot'),
            checkboxGroupInput("ids", "Individuals available for genotyping")),
     column(6,
-           selectInput("pedTrue", "True pedigree", defaultPedigrees),
+           selectInput("pedTrue", "True pedigree", defaultPedigrees, width = '100%'),
+           HTML('<p class="text-center">- or -</p>'),
+           fileInput('pedTrueFile', ".ped file",  width = '100%'),
            plotOutput('pedTruePlot'))
+  ),
+  fluidRow(
+    column(12,
+           h3('Step 2. Provide available genetic data'))
+  ),
+  fluidRow(
+    column(4,
+           fileInput('frequencyDbFile', 'Allele frequency database', width = '100%'),
+           textOutput('frequencyDbSummary')),
+    column(4,
+           fileInput('referenceFiles', 'Reference files', width = '100%'))
   )
 ))
