c2007e8f413696c4544f98089e40a873918015eb
Elias Hernandis <eliashernandis@gmail.com>
2019-07-16 20:02:18 +0200

    Rename relMix import adapter to generic import adapter.

--- inst/exclusionPowerUI/global.R
+++ inst/exclusionPowerUI/global.R
@@ -2,7 +2,7 @@ source('defaultPedigreeDefinitions.R')
 
 source('advancedTableFileLoader.R')
 
-source('importAdapters/relMix-import-adapter.R')
+source('importAdapters/generic-import-adapter.R')
 
 getMarkerNames = function(ped) {
   as.vector(lapply(ped$markerdata, function(item) attr(item, 'name')), mode = 'character')
