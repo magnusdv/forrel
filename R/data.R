df189f35f5f6a91b56806812d112fac9f1584ec1
Magnus Dehli Vigeland <magnusdv@medisin.uio.no>
2020-05-21 12:30:54 +0200

    Add `NorwegianFrequencies` dataset
    * Add documentation and source

---
+++ R/data.R
@@ -0,0 +1,86 @@
+#' Norwegian STR frequencies
+#'
+#' A database of Norwegian allele frequencies for 35 STR markers. This database
+#' is also available in the R package `Familias`.
+#'
+#' @format A list of length 35. Each entry is a numerical vector summing to 1,
+#'   named with allele labels.
+#'
+#'   The following markers are included:
+#'
+#'   * `D3S1358`: 12 alleles
+#'
+#'   * `TH01`: 10 alleles
+#'
+#'   * `D21S11`: 26 alleles
+#'
+#'   * `D18S51 `: 23 alleles
+#'
+#'   * `PENTA_E`: 21 alleles
+#'
+#'   * `D5S818`: 9 alleles
+#'
+#'   * `D13S317`: 9 alleles
+#'
+#'   * `D7S820`: 19 alleles
+#'
+#'   * `D16S539`: 9 alleles
+#'
+#'   * `CSF1PO`: 11 alleles
+#'
+#'   * `PENTA_D`: 24 alleles
+#'
+#'   * `VWA`: 12 alleles
+#'
+#'   * `D8S1179`: 12 alleles
+#'
+#'   * `TPOX`: 9 alleles
+#'
+#'   * `FGA`: 25 alleles
+#'
+#'   * `D19S433`: 17 alleles
+#'
+#'   * `D2S1338`: 13 alleles
+#'
+#'   * `D10S1248`: 9 alleles
+#'
+#'   * `D1S1656`: 17 alleles
+#'
+#'   * `D22S1045`: 9 alleles
+#'
+#'   * `D2S441`: 13 alleles
+#'
+#'   * `D12S391`: 23 alleles
+#'
+#'   * `SE33`: 55 alleles
+#'
+#'   * `D7S1517`: 11 alleles
+#'
+#'   * `D3S1744`: 8 alleles
+#'
+#'   * `D2S1360`: 10 alleles
+#'
+#'   * `D6S474`: 6 alleles
+#'
+#'   * `D4S2366`: 7 alleles
+#'
+#'   * `D8S1132`: 12 alleles
+#'
+#'   * `D5S2500`: 8 alleles
+#'
+#'   * `D21S2055`: 18 alleles
+#'
+#'   * `D10S2325`: 10 alleles
+#'
+#'   * `D17S906`: 78 alleles
+#'
+#'   * `APOAI1`: 41 alleles
+#'
+#'   * `D11S554`: 51 alleles
+#'
+#' @source Dupuy et al. (2013): *Frequency data for 35 autosomal STR markers in
+#'   a Norwegian, an East African, an East Asian and Middle Asian population and
+#'   simulation of adequate database size*. Forensic Science International:
+#'   Genetics Supplement Series, Volume 4 (1).
+#'
+"NorwegianFrequencies"
---
+++ data-raw/NorwegianFrequencies.R
@@ -0,0 +1,5 @@
+## code to prepare `NorwayFreqSTR` dataset goes here
+
+library(Familias)
+data("NorwegianFrequencies")
+usethis::use_data(NorwegianFrequencies, overwrite = TRUE)
