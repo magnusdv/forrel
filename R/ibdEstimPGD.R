421d8609da2434c85c47cf5fcfe9d774db2af113
Magnus Dehli Vigeland <magnusdv@medisin.uio.no>
2021-03-12 13:52:28 +0100

    Clean up IBD estimation code.
    * Delete `ibdEstim()`
    * Reorganise

--- R/ibdEstimPGD.R
+++ R/ibdEstimPGD.R
@@ -288,6 +288,107 @@ as.double.ibdEst = function(x, ...) {
   list(estimate = xk, loglik = LL$loglik, iterations = k, ids = pair, nMarkers = sum(keep))
 }
 
+# Prepare data for fast computation of IBD likelihood
+# Input: ids = a vector of ID labels
+# Output: List of lists. For each indiv, the output contains a list of 4 vectors:
+#   a1: first allele
+#   a2: second allele
+#   f1: frequency of first allele
+#   f2: frequency of second allele
+.getAlleleData2 = function(x, ids) {
+  nMark = nMarkers(x)
+  nSeq = seq_len(nMark)
+  ids = as.character(ids)
+
+  if(is.pedList(x)) {
+    pednr = getComponent(x, ids, checkUnique = TRUE)
+    if(all(pednr == pednr[1]))
+      x = x[[pednr[1]]]
+  }
+
+  if(is.pedList(x)) {
+    alsMat = do.call(rbind, lapply(x, function(cmp) matrix(unlist(cmp$MARKERS), ncol = 2*nMark)))
+    freqlist = lapply(nSeq, function(i) unname(afreq(x, i))) # checks consistency!
+  }
+  else {
+    alsMat = matrix(unlist(x$MARKERS), ncol = 2*nMark)
+    freqlist = lapply(x$MARKERS, function(m) attr(m, "afreq")) # faster than the generic afreq()
+  }
+
+  rownames(alsMat) = unlist(labels(x))
+  alsMat = alsMat[ids, , drop = FALSE]
+
+  maxAlNum = max(lengths(freqlist))
+  freqMat = matrix(unlist(lapply(freqlist, `length<-`, maxAlNum),
+                          recursive = FALSE, use.names = FALSE),
+                   ncol = nMark)
+
+  even = 2 * nSeq
+  odd = even - 1
+
+  res = lapply(ids, function(id) {
+    a1 = alsMat[id, odd]
+    a2 = alsMat[id, even]
+    a1nonz = a1 > 0
+    a2nonz = a2 > 0
+    f1 = f2 = rep(NA_real_, nMark)
+    f1[a1nonz] = freqMat[cbind(a1, nSeq)]
+    f2[a2nonz] = freqMat[cbind(a2, nSeq)]
+    list(a1 = a1, a2 = a2, f1 = f1, f2 = f2)
+  })
+
+  names(res) = ids
+  res
+}
+
+
+# Input: alleleData = ouput from .getAlleleData() for a pair of indivs
+.likelihoodWeights = function(alleleData, param = "kappa") {
+  .a = alleleData[[1]]$a1
+  .b = alleleData[[1]]$a2
+  .c = alleleData[[2]]$a1
+  .d = alleleData[[2]]$a2
+  pa = alleleData[[1]]$f1
+  pb = alleleData[[1]]$f2
+  pc = alleleData[[2]]$f1
+  pd = alleleData[[2]]$f2
+
+  homoz1 = .a == .b
+  homoz2 = .c == .d
+  mac = .a == .c
+  mbc = .b == .c
+  mad = .a == .d
+  mbd = .b == .d
+  g1.fr = 2^(!homoz1) * pa * pb
+  g2.fr = 2^(!homoz2) * pc * pd
+
+  if(param == "kappa") {
+    # Matrix with 3 rows: conditional probs given UN, PO, MZ
+    w = matrix(0, nrow = 3, ncol = length(.a))
+    w[1, ] = g1.fr * g2.fr                  # Prob(g1, g2 | UN)
+    w[2, ] = (.5)^(homoz1+homoz2)*pa*pb*(pd*(mac+mbc) + pc*(mad+mbd)) # Prob(g1, g2 | PO)
+    w[3, ] = g1.fr * ((mac & mbd) | (mad & mbc)) # Prob(g1, g2 | MZ)
+  }
+  else if(param == "delta") {
+    # Matrix with 9 rows: Row i are the conditional probs given state Ji
+    w = matrix(0, nrow = 9, ncol = length(.a))
+    w[1, ] = pa * (homoz1 & homoz2 & mac)   # Prob(g1, g2 | J1)
+    w[2, ] = pa * pc * (homoz1 & homoz2)    # Prob(g1, g2 | J2)
+    w[3, ] = pa * homoz1 * (mac *pd + mad * pc) * (.5)^homoz2 # Prob(g1, g2 | J3)
+    w[4, ] = pa * g2.fr * homoz1            # Prob(g1, g2 | J4)
+    w[5, ] = pc * homoz2 * (mac *pb + mbc * pa) * (.5)^homoz1 # Prob(g1, g2 | J5)
+    w[6, ] = g1.fr * pc * homoz2            # Prob(g1, g2 | J6)
+    w[7, ] = g1.fr * ((mac & mbd) | (mad & mbc)) # Prob(g1, g2 | MZ)
+    w[8, ] = (.5)^(homoz1+homoz2)*pa*pb*(pd*(mac+mbc) + pc*(mad+mbd)) # Prob(g1, g2 | PO)
+    w[9, ] = g1.fr * g2.fr                  # Prob(g1, g2 | UN)
+  }
+  else
+    stop2("`param` must be either 'kappa' or 'delta': ", param)
+
+  w
+}
+
+
 stationary = function(p, grad, tol) {
   # print(grad,digits = 12)
   # print(sapply(seq_along(p), function(i) {pi = p; pi[i] = p[i] - 1; grad %*% pi}))
@@ -322,6 +423,9 @@ simplexProject = function(y) {
   if(y[1] < y[2]) {tmp = y[1]; y[1] = y[2]; y[2] = tmp}
   y
 }
+
+
+
 
 # Plot contour lines for the ML function when estimating kappa.
 # This function is (optionally) called from within `ibdEstimate()`.
