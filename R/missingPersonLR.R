33c163bee28b0765280f1abf0f8346d64caa2fcb
Magnus Dehli Vigeland <magnusdv@medisin.uio.no>
2022-04-21 10:34:21 +0200

    Make `missingPersonLR()` more user friendly

--- R/missingPersonLR.R
+++ R/missingPersonLR.R
@@ -6,9 +6,8 @@
 #'
 #' @param reference A `ped` object with attached markers.
 #' @param missing The ID label of the missing member of `reference`.
-#' @param poi A `singleton` object with genotypes for the person of interest.
-#'   The marker names of `poi` must coincide with those of `reference`. The ID
-#'   label of `poi` is irrelevant.
+#' @param poi A `singleton` object, or NULL. If NULL, and `missing` is
+#'   genotyped, this data is extracted and used as `poi`.
 #'
 #' @return The `LRresult` object returned by [kinshipLR()], but without the
 #'   trivial `H2:H2` comparison.
@@ -43,7 +42,7 @@
 #' ### Person of interest 2: The true MP
 #'
 #' # Simulate MP conditional on reference, and extract as singleton
-#' poi2 = profileSim(x, N = 1, ids = "MP")[[1]]
+#' poi2 = profileSim(x, N = 1, ids = c("2", "MP"))[[1]]
 #'
 #' # Extract MP as singleton
 #' poi2 = subset(poi2, "MP")
@@ -54,7 +53,7 @@
 #' lr2$LRperMarker
 #'
 #' @export
-missingPersonLR = function(reference, missing, poi) {
+missingPersonLR = function(reference, missing, poi = NULL, verbose = TRUE) {
 
   if(is.pedList(reference))
     stop2("Argument `reference` must be a connected pedigree, not a list of pedigrees")
@@ -62,17 +61,51 @@ missingPersonLR = function(reference, missing, poi) {
   if(!is.ped(reference))
     stop2("Argument `reference` must be a `ped` object")
 
-  if(!is.singleton(poi))
-    stop2("Argument `poi` must be a singleton")
+  # Case I: poi data is attached to MP in the pedigree
+  if(is.null(poi)) {
 
-  poiName = labels(poi)
+    if(!missing %in% typedMembers(reference))
+      stop2(sprintf("Interpreting `%s` as POI, but this individual is not typed", missing))
 
-  # Hypothesis 1: POI = MP
-  H1 = relabel(reference, old = missing, new = poiName)
-  H1 = transferMarkers(from = poi, to = H1, erase = FALSE)
+    if(verbose)
+      cat(sprintf("Interpreting `%s` as the person of interest\n", missing))
 
-  # Hypothesis 2: POI unrelated to MP
-  H2 = list(reference, poi)
+    # Hyp1
+    if(verbose)
+      cat(sprintf("\nForming H1 from reference:\n  * Renaming `%s` to `POI`\n", missing))
+    H1 = relabel(reference, old = missing, new = "POI")
+
+    # Hyp2
+    if(verbose)
+      cat(sprintf("\nForming H2 from reference:\n  * Extracting `%s` as singleton named `POI`\n", missing))
+    poiSingleton = subset(reference, missing) |>
+      relabel(old = missing, new = "POI")
+    H2 = list(reference, poiSingleton)
+  }
+  else {
+    if(!is.singleton(poi))
+      stop2("Argument `poi` must be a singleton or NULL: ", poi)
+
+    poiName = labels(poi)
+
+    # Hypothesis 1: POI = MP
+    if(verbose)
+      cat(sprintf("\nForming H1 from reference:\n  * Renaming `%s` to `%s`\n  * Transferring genotypes\n", missing, poiName))
+    H1 = relabel(reference, old = missing, new = poiName)
+    H1 = transferMarkers(from = poi, to = H1, erase = FALSE)
+
+    # Hypothesis 2: POI unrelated to MP
+    if(verbose)
+      cat("\nForming H2 from reference:\n  * List of reference and poi singleton\n")
+    H2 = list(reference, poi)
+  }
+
+  if(missing %in% typedMembers(H2)) {
+    if(verbose)
+      cat(sprintf("  * Removing genotypes from `%s`\n", missing))
+    H2 = setAlleles(H2, missing, alleles = 0)
+  }
+
 
   # Calculate LR
   lr = kinshipLR(H1, H2, ref = 2, verbose = FALSE)
