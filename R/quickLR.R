9826ce80e44fbd33272b1b260ea4ef1452395cc0
Magnus Dehli Vigeland <magnusdv@medisin.uio.no>
2025-12-22 11:40:15 +0100

    Add hypothesis "cous" (1st cousins) to `quickLR()`

--- R/quickLR.R
+++ R/quickLR.R
@@ -1,15 +1,15 @@
 #' LR calculations for paternity and sibship
 #'
 #' A thin wrapper around [kinshipLR()] for the common scenario of testing a pair
-#' of individuals for paternity and/or sibship, against being unrelated.
+#' of individuals for paternity or sibship against being unrelated.
 #'
-#' @param x A ped object or a list of such.
+#' @param x A `ped` object or a list of such.
 #' @param ids A vector of two typed members of `x`. If not given, the typed
 #'   members of `x` are selected by default, but note that this gives an error
 #'   if the number of such individuals is not 2.
-#' @param test The hypotheses to be tested (against 'unrelatedness'). Allowed
-#'   values are "pat" (=paternity), "sib" (=full siblings), and "half" (=half
-#'   siblings). By default, all three are included.
+#' @param test The hypotheses to be tested against unrelatedness. Allowed values
+#'   are "pat" (= paternity), "sib" (= full siblings), "half" (= half siblings)
+#'   and "cous" (= first cousins). By default, all are included.
 #'
 #' @return A (slightly simplified) `LRresult` object, as described in
 #'   [kinshipLR()].
@@ -18,19 +18,21 @@
 #' # Simulate 100 markers for half siblings
 #' x = halfSibPed() |> markerSim(N = 100, ids = 4:5, alleles = 1:3, seed = 1)
 #'
-#' # Test paternity, full sib, half sib
+#' # Test for paternity, full sibs, half sibs, 1st cousins
 #' quickLR(x)
 #'
 #' @export
-quickLR = function(x, ids = typedMembers(x), test = c("pat", "sib", "half")) {
+quickLR = function(x, ids = typedMembers(x), test = c("pat", "sib", "half", "cous")) {
   if(length(ids) != 2)
     stop2("Please indicate exactly two individuals. Received: ", ids)
 
   if(!all(ids %in% typedMembers(x)))
     stop2("Individual is not typed: ", setdiff(ids, typedMembers(x)))
 
-  if(!all(test %in% c("pat", "sib", "half")))
-    stop2("Illegal value of `test` (allowed are 'pat', 'sib', 'half'): ", setdiff(test, c("pat", "sib", "half")))
+  rels = c("pat", "sib", "half", "cous")
+  if(!all(test %in% rels))
+    stop2("Illegal value of `test`: ", setdiff(test, rels),
+          "\nAllowed inputs are 'pat', 'sib', 'half', 'cous'")
 
   sex = getSex(x, ids)
 
@@ -40,7 +42,9 @@ quickLR = function(x, ids = typedMembers(x), test = c("pat", "sib", "half")) {
                       children = ids[2], sex = sex[2]),
     sib  = nuclearPed(father = "_fa_", mother = "_mo_", children = ids, sex = sex),
     half = halfSibPed(sex1 = sex[1], sex2 = sex[2]) |>
-      relabel(old = 1:5, new = c("_mo1_", "_fa_", "_mo2_", ids))
+      relabel(old = 1:5, new = c("_mo1_", "_fa_", "_mo2_", ids)),
+    cous = cousinPed(1) |> setSex(7:8, sex = sex) |>
+      relabel(old = 1:8, new = c(paste0("_nn_", 1:6), ids))
   )[test]
 
   peds$unrel = transferMarkers(from = x, to = singletons(ids, sex = sex))
